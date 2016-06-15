0183b4a63812a7a1226f405d166ee9791a2a8f7d
Mastere 1 IT <mastere1it@iMacdeAutres46.fro-206.froment.in.iesanetwork.com>
2016-06-15 14:23:19 +0200

    Add var and const

---
+++ swift/MonAirDeJeu.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playgrounde"
+print(str)
+
+
+// 2
+
+var myVariable = 42
+
+myVariable = 50
+
+var myOtherVariable :Double
+
+
+// 03 
+
+let implicitInteger = 70
+
+let implicitDouble = 70.0
+
+let explicitDouble :Double = 70
+
+
+
+
+
+
+
