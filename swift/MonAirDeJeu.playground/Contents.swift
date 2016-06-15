4620adc35aaa1f68605c0e1910b475f7e248d5b0
Mastere 1 IT <mastere1it@iMacdeAutres46.fro-206.froment.in.iesanetwork.com>
2016-06-15 17:55:39 +0200

    #5 d'autres trucs

--- swift/MonAirDeJeu.playground/Contents.swift
+++ swift/MonAirDeJeu.playground/Contents.swift
@@ -194,12 +194,26 @@ range.2
 
 // 15
 
-//func calculateStatistics(scores:[Int]) -> (min: Int, max: Int, sum: Int){
-//    var min = scores[0]
-//    var max = scores[0]
-//    var sum = 0
-//    for score in scores {
-//        if score > max { max = score } else if score < min {min = score}
-//        
-//    }
-//}
+func calculateStatistics(scores:[Int]) -> (min: Int, max: Int, sum: Int){
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+    for score in scores {
+        if score > max { max = score } else if score < min {min = score}
+        sum += score
+    }
+    return (min, max, sum)
+}
+let statistics = calculateStatistics([5, 3, 100, 3 , 9])
+print(statistics.sum)
+print(statistics.2)
+
+// 16 
+
+func sumOf(numbers: Int...) -> Int{
+    var sum = 0
+    for number in numbers {sum += number}
+    return sum
+}
+sumOf()
+sumOf(42, 597, 12)
