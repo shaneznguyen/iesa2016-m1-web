9f126cca597a796963a8f309d6788f0131c53398
Mastere 1 IT <mastere1it@iMacdeAutres5.sto-002.saint-augustin.in.iesanetwork.com>
2016-06-20 10:47:06 +0200

    #6 New playground

--- swift/ObjectPlaygrond.playground/Contents.swift
+++ swift/ObjectPlaygrond.playground/Contents.swift
@@ -2,4 +2,17 @@
 
 import UIKit
 
-var str = "Hello, playground"
+class Shape {
+    var sideCount = 0
+    func getInfo() -> String {
+        return "A shape with \(sideCount) sides."
+    }
+}
+
+var shape = Shape()
+
+shape.sideCount = 5
+
+shape.getInfo()
+
+var displayInfo = shape.getInfo()
